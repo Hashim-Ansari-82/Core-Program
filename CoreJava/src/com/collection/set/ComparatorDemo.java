0ac36411a3e310768d4abbf5e1e6247a5bea4c84
Hashim Ansari <mohammadhashim7922@gmail.com>
2025-12-12 14:56:24 +0530

    updation

---
+++ CoreJava/src/com/collection/set/ComparatorDemo.java
@@ -0,0 +1,37 @@
+package com.collection.set;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+class TreeSet1st {
+    public static void main(String[] args) {
+
+        TreeSet<Integer> ts = new TreeSet<Integer>(new ComparatorDemo());
+        ts.add(23);
+        ts.add(54);
+        ts.add(12);
+        ts.add(40);
+        ts.add(25);
+        ts.add(3);
+        System.out.println(ts);
+    }
+
+}
+public class ComparatorDemo implements Comparator {
+     
+    public int compare(Object obj1,Object obj2){
+
+        Integer i=(Integer)obj1;
+        Integer i1=(Integer)obj2;
+        if(i<i1){
+            return +1;
+        }
+        else if(i>i1){
+            return -1;
+        }
+        else{
+            return 0;
+        }
+    }
+    
+}
---
+++ CoreJava/src/com/collection/set/TreeSetDemo.java
@@ -0,0 +1,16 @@
+package com.collection.set;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+    public static void main(String[] args) {
+
+        TreeSet<String> t=new TreeSet<>();
+
+        t.add("String");
+        t.add("Koilsa");
+        t.add("Rani");
+        t.add("Hashim");
+        System.out.println(t);
+    }
+}
