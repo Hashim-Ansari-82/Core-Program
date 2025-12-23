f1c3bcbdcd631c631eec2ccaa6b86f16c066a95e
Hashim Ansari <mohammadhashim7922@gmail.com>
2025-12-23 11:45:28 +0530

    Delete CoreJava/src/Main.java

--- CoreJava/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int count=0;
-        while(true){
-            System.out.println(++count +"----> Ammara");
-        }
-    }
-}
